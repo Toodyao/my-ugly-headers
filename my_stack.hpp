77df0e6cf0e3235f1ef4bac4c1113118d5b983a9
Toodyao-XPS-13 <toodyao@gmail.com>
2018-03-14 11:58:59 +0800

    finish my_stack

--- my_stack.hpp
+++ my_stack.hpp
@@ -3,4 +3,34 @@
 
 #include "my_singly_list.hpp"
 
+template<typename T>
+class Mystack : public Mylist<T> {
+
+public:
+	Mystack() { }
+
+	~Mystack() { }
+
+	void push(T value) {
+		this->push_front(value);
+	}
+
+	void pop() {
+		this->pop_front();
+	}
+
+	T top() {
+		// check empty() before use!
+		return this->list_head->data;
+	}
+
+	bool empty() {
+		return this->empty();
+	}
+
+	int size() {
+		return this->size();
+	}
+};
+
 #endif // _MY_STACK_
