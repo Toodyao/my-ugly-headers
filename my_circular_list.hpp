cd78db981c538310b9d571a0e2f22bde5f494f15
Toodyao-XPS-13 <toodyao@gmail.com>
2018-03-14 23:25:20 +0800

    fix next()

---
+++ my_circular_list.hpp
@@ -0,0 +1,19 @@
+#ifndef _MY_CIRCULAR_LIST_
+#define _MY_CIRCULAR_LIST_
+
+#include "my_singly_list.hpp"
+
+template<typename T>
+class Circularlist : public Mylist<T> {
+public:
+	Circularlist()  { }
+	~Circularlist() { }
+
+	typename Mylist<T>::node *next() {
+	return Mylist<T>::iterator != nullptr ? 
+		Mylist<T>::iterator->next : Mylist<T>::list_head;
+	}
+
+};
+
+#endif // _MY_CIRCULAR_LIST_
