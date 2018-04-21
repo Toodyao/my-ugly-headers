92b0287ade00737dcd15ad39da823614ebcd2b01
Toodyao-XPS-13 <toodyao@gmail.com>
2018-04-21 12:07:50 +0800

    add tree and expression headers

---
+++ binary_tree.hpp
@@ -0,0 +1,131 @@
+#ifndef DS_TREE_BINARY_TREE_HPP
+#define DS_TREE_BINARY_TREE_HPP
+
+#include "tree_node.hpp"
+#include <ostream>
+#include <queue>
+
+template <typename T>
+class BinaryTree : public TreeNode<T> {
+private:
+	TreeNode<T> *root_node;
+	int max(int a, int b);
+public:
+	BinaryTree();
+	explicit BinaryTree(TreeNode<T> * root);
+
+	virtual ~BinaryTree();
+
+	inline TreeNode<T> *root();
+	inline T data(TreeNode<T> * node);
+
+	void pre_order(TreeNode<T>* t);
+	void in_order(TreeNode<T>* t);
+	void post_order(TreeNode<T>* t);
+	void level_order();
+
+	int get_tree_height(TreeNode<T> *);
+
+	void delete_subtree(TreeNode<T> *t);
+};
+
+template<typename T>
+BinaryTree<T>::BinaryTree() {
+	root_node = new TreeNode<T>;
+}
+
+template<typename T>
+BinaryTree<T>::BinaryTree(TreeNode<T> *root) {
+	root_node = root;
+}
+
+template<typename T>
+BinaryTree<T>::~BinaryTree() {
+	delete_subtree(root_node);
+}
+
+template<typename T>
+int BinaryTree<T>::max(const int a, const int b) {
+	return a > b ? a : b;
+}
+
+template<typename T>
+TreeNode<T> * BinaryTree<T>::root() {
+	return root_node;
+}
+
+template<typename T>
+T BinaryTree<T>::data(TreeNode<T> *node) {
+	return node->get_data();
+}
+
+template<typename T>
+void BinaryTree<T>::pre_order(TreeNode<T>* t) {
+	if (t == nullptr)
+		return;
+	else {
+		std::cout << t->get_data() << std::endl;
+		pre_order(t->left_child());
+		pre_order(t->right_child());
+	}
+
+}
+
+template<typename T>
+void BinaryTree<T>::in_order(TreeNode<T>* t) {
+	if (t == nullptr)
+		return;
+	else {
+		in_order(t->left_child());
+		std::cout << t->get_data() << std::endl;
+		in_order(t->right_child());
+	}
+}
+
+template<typename T>
+void BinaryTree<T>::post_order(TreeNode<T>* t) {
+	if (t == nullptr)
+		return;
+	else {
+		post_order(t->left_child());
+		post_order(t->right_child());
+		std::cout << t->get_data() << std::endl;
+	}
+}
+
+template<typename T>
+void BinaryTree<T>::level_order() {
+	std::queue<TreeNode<T>* > q;
+	q.push(root_node);
+	while (!q.empty()) {
+		TreeNode<T>* temp = q.front();
+		q.pop();
+		if (temp == nullptr)
+			continue;
+		std::cout << temp->get_data() << " ";
+		q.push(temp->left_child());
+		q.push(temp->right_child());
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+int BinaryTree<T>::get_tree_height(TreeNode<T> *root) {
+	if (root == nullptr)
+		return 0;
+	return max(get_tree_height(root->left_child())+1, get_tree_height(root->right_child())+1);
+}
+
+template<typename T>
+void BinaryTree<T>::delete_subtree(TreeNode<T> *t) { // post order delete
+	if (t != nullptr) {
+		delete_subtree(t->left_child());
+		delete_subtree(t->right_child());
+//		std::cout << "deleted " << t->get_data() << std::endl;
+		delete t;
+		return;
+	}
+}
+
+
+#endif //DS_TREE_BINARY_TREE_HPP
---
+++ tree_node.hpp
@@ -0,0 +1,88 @@
+#ifndef DS_TREE_TREE_NODE_HPP
+#define DS_TREE_TREE_NODE_HPP
+
+template <typename T>
+class TreeNode {
+private:
+	T data;
+	TreeNode<T> *lchild;
+	TreeNode<T> *rchild;
+	TreeNode<T> *parent;
+
+public:
+	TreeNode();
+	explicit TreeNode(T d);
+	TreeNode(T d, TreeNode<T> *l, TreeNode<T> *r);
+	TreeNode(T d, TreeNode<T> *l, TreeNode<T> *r, TreeNode<T> *p);
+
+//	inline TreeNode<T> *root();
+	inline TreeNode<T> * left_child();
+	inline TreeNode<T> * right_child();
+
+	inline void add_left(TreeNode<T> *t);
+	inline void add_right(TreeNode<T> *t);
+	inline void add_left(T d);
+	inline void add_right(T d);
+
+	inline T get_data();
+	inline void set_data(T d);
+};
+
+template<typename T>
+TreeNode<T>::TreeNode() :
+		lchild(nullptr), rchild(nullptr), parent(nullptr) { }
+
+template<typename T>
+TreeNode<T>::TreeNode(T d) :
+		data(d), lchild(nullptr), rchild(nullptr), parent(nullptr) { }
+
+template<typename T>
+TreeNode<T>::TreeNode(T d, TreeNode<T> *l, TreeNode<T> *r) :
+		data(d), lchild(l), rchild(r), parent(nullptr) { }
+
+template<typename T>
+TreeNode<T>::TreeNode(T d, TreeNode<T> *l, TreeNode<T> *r, TreeNode<T> *p) :
+		data(d), lchild(l), rchild(r), parent(p) { }
+
+template<typename T>
+TreeNode<T> * TreeNode<T>::left_child() {
+	return lchild;
+}
+
+template<typename T>
+TreeNode<T> * TreeNode<T>::right_child() {
+	return rchild;
+}
+
+template<typename T>
+T TreeNode<T>::get_data() {
+	return data;
+}
+
+template<typename T>
+void TreeNode<T>::set_data(T d) {
+	data = d;
+}
+
+template<typename T>
+void TreeNode<T>::add_left(TreeNode<T> *t) {
+	lchild = t;
+}
+
+template<typename T>
+void TreeNode<T>::add_right(TreeNode<T> *t) {
+	rchild = t;
+}
+
+template<typename T>
+void TreeNode<T>::add_left(T d) {
+	lchild = new TreeNode(d);
+}
+
+template<typename T>
+void TreeNode<T>::add_right(T d) {
+	rchild = new TreeNode(d);
+}
+
+
+#endif //DS_TREE_TREE_NODE_HPP
